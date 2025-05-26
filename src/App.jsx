222c822f8134df845b11c60669e75798bfb208f3
Amar AMEDJKANE <amedjkane.amar02@gmail.com>
2025-05-26 12:55:29 +0200

    new commit to RFront with chnages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Roulette from "./roulette/roulette";
+import Formulaire from "./formulaire/formulaire"; 
 
 
 
 const App = ()=>{
   return (
+<div>
+  <Roulette/>
+<Formulaire/>
+</div>
 
-<Roulette/>
   )
 };
 
---
+++ src/formulaire/formulaire.jsx
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import './formulaire.css';
+{/* import { Link } from 'react-router-dom';
+  */}
+
+const Formulaire = () => {
+
+const[nom, setNom] =useState("");
+const[prenom, setPrenom] =useState("");
+const[email, setEmail] =useState("");
+
+
+
+
+
+
+
+
+
+  return (
+    <div id="formulaire">
+      <div className="container">
+        <div className="heading">Informations</div>
+        <form action="" className="form">
+
+          <div className="input-wrapper">
+          <input  onInput={(e)=>{  setNom(e.target.value);}} value={nom} required className={`input`} type="text" name="nom" id="nom" placeholder="Nom" />
+  <span   onClick={()=>{setNom("")}} className={`clear-icon  ${nom? "show" : ""}`}>×</span>
+</div> 
+
+
+
+                    <div className="input-wrapper">
+                    <input onInput={(e)=>{  setPrenom(e.target.value);}} value={prenom}  required className="input" type="text" name="prenom" id="prenom" placeholder="Prénom" />
+  <span onClick={()=>{setPrenom("")}} className={`clear-icon  ${prenom? "show" : ""}`}>×</span>
+</div> 
+
+<div className="input-wrapper">
+  <input required onInput={(e)=>{  setEmail(e.target.value);}} value={email} className="input" type="email" name="email" id="email" placeholder="Email" />
+  <span onClick={()=>{setEmail("")}} className={`clear-icon  ${email? "show" : ""}`}>×</span>
+</div>         
+
+
+{/* 
+<Link to="/roulette">
+          <input className="login-button" type="submit" value="Valider et passer à la roulette" />
+          </Link>
+*/}
+ 
+        </form>
+
+        
+
+
+        
+      </div>
+    </div>
+  );
+};
+
+export default Formulaire;
