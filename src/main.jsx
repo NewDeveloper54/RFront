bc453322b8d61ef63ce5cc7125b9a41531edf05d
Amar AMEDJKANE <amedjkane.amar02@gmail.com>
2025-05-26 14:06:33 +0200

    new commit to RFront with chnages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter } from 'react-router-dom';
+
+
 
 
 const rootElement = document.querySelector("#root");
 const root = createRoot(rootElement);
 
 root.render(
-< App/>
-)
+ <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    )
+
+
