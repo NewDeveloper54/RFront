9e1d43f380556211cb3143d81547724349d0d655
Amar AMEDJKANE <amedjkane.amar02@gmail.com>
2025-05-27 11:41:48 +0200

    changes in RFront now4

--- src/roulette/roulette.jsx
+++ src/roulette/roulette.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './roulette.css';
-import hoody from "../assets/hoodyBG.png";
+import hoody from "../assets/hoodieBG.png";
 import trophy from "../assets/trophee.png";
 import headphones from "../assets/headphones.png";
 import surpris from "../assets/surpris.png";
+import tShirt from "../assets/t-shirt.png";
+import mug from "../assets/mug.png";
+import socks from "../assets/socks.png";
+import cup from "../assets/cup.png";
+import chapeau from "../assets/chapeau.png";
 import { Link } from 'react-router-dom';
 
 const Roulette = () => {
+
+
+
+
+
   const [isShown, setIsShown] = useState("");
   const [isDown, setIsDown] = useState(false);
   const [rotation, setRotation] = useState(0);
@@ -18,21 +28,37 @@ const Roulette = () => {
   const infos = {
     0: { img: hoody, title: "hoody", class: "hoody" },
     1: { img: surpris, title: "Oops", class: "oops" },
-    2: { img: headphones, title: "headphones", class: "headphones" },
-    3: { img: surpris, title: "oops", class: "oops" },
-    4: { img: hoody, title: "fdfdfd", class: "hoody" },
-    5: { img: surpris, title: "oops", class: "oops" },
-    6: { img: hoody, title: "g", class: "hoody" } ,
-    7: { img: surpris, title: "oops", class: "oops" },
-    8: { img: headphones, title: "headphones", class: "headphones" },
+    2: { img: headphones, title: "casque", class: "headphones" },
+    3: { img: surpris, title: "Oops", class: "oops" },
+    4: { img: tShirt, title: "t-shirt", class: "tShirt" },
+    5: { img: surpris, title: "Oops", class: "oops" },
+    6: { img: cup, title: "goblet", class: "cup" } ,
+    7: { img: surpris, title: "Oops", class: "oops" },
+    8: { img: chapeau, title: "chapeau", class: "chapeau" },
     9: { img: surpris, title: "Oops", class: "oops" },
-    10: { img: hoody, title: "fdfdfd", class: "hoody" },
+    10: { img: mug, title: "tasse", class: "mug" },
     11: { img: surpris, title: "Oops", class: "oops" },
-    12: { img: hoody, title: "fdfdfd", class: "hoody" },
+    12: { img: socks, title: "chaussettes", class: "socks", pclass: "psocks" },
     13: { img: surpris, title: "Oops", class: "oops" },
     14: { img: trophy, title: "Roi de la journée", class: "trophy" },
     15: { img: surpris, title: "Oops", class: "oops" } ,
   };
+
+  const [value, setValue] = useState("");
+
+
+useEffect(() => {
+  const current = infos[winnerIndex];
+
+  if (!current) return; 
+
+  if (current.title === "Oops") {
+    setValue("Dommage pour cette fois!");
+  } else {
+    setValue(`Vous avez gagné : ${current.title}`);
+  }
+}, [winnerIndex]);
+
 
   const handleSpin = () => {
     if (isSpinning) return;
@@ -71,32 +97,43 @@ const Roulette = () => {
           >
             {segments.map((_, i) => {
               const info = infos[i];
-              const isLose = i >= 8;
+              const isLose = i % 2 ===1;
               const angle = i * (360 / 16);
               const translateY = isLose ? 250 : 250;
               const extraStyle = isLose
-                ? { transform: `rotate(${angle }deg) translateY(${translateY}px) translateX(-20px)` }
-                : { transform: `rotate(${angle  }deg) translateY(${translateY}px) translateX(20px)` };
+                ? { transform: `rotate(${angle}deg) translateY(${translateY}px) translateX(-20px)` }
+                : { transform: `rotate(${angle}deg) translateY(${translateY}px) translateX(20px)` };
 
               return (
                 <div
                   className={`triangle ${isLose ? "lose" : ""}`}
                   style={extraStyle}
                   key={i}
+
                 >
                   <div className={isLose ? "shapeLose lose-shape" : "shape"}>
                     <p
-                      className={isLose ? "lose-text" : "p"}
+className={`${isLose ? "lose-text" : "p"} ${info.pclass}`}
                       style={
                         isLose
                           ? {
+                            transform: "rotate(180deg)",
                               fontSize: "10px",
                               fontWeight: "bold",
                               fontFamily: "arial",
                               width: "50px",
                               textAlign: "center",
                             }
-                          : {}
+                          : { 
+                                                        transform: "rotate(180deg)",
+
+                            fontSize: "10px",
+                              fontWeight: "bold",
+                              fontFamily: "arial",
+                              width: "40px",
+                              textAlign: "center",
+                              
+                           }
                       }
                     >
                       {info.title}
@@ -123,7 +160,8 @@ const Roulette = () => {
 
         {winnerIndex !== null && !isSpinning && (
           <div className="result">
-            <h2>Bravo ! Vous avez gagné : {infos[winnerIndex].title}</h2>
+            
+            <h2 style={{fontFamily:"arial", color:"white"}}> {value}  </h2>
           </div>
         )}
 
