1a2eec4a522fb73aaf45a645281056c436e98847
Amar AMEDJKANE <amedjkane.amar02@gmail.com>
2025-06-02 14:03:29 +0200

    changes in RFront now joueur

--- src/formulaire/formulaire.jsx
+++ src/formulaire/formulaire.jsx
@@ -109,7 +109,7 @@ const Formulaire = () => {
         </form>
       </div>
       <div style={{ display: "flex", justifyContent: "flex-end", padding: "15px",
-         position:"absolute",left:"80%",bottom:"80%",
+         position:"absolute",left:"75%",bottom:"95%",
          height:"60px", width:"200px"}}>
   <button
     onClick={() => navigate("/joueurs")}
